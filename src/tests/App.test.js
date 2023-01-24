c5fb587480fce280ceefe3b4b350d2a21c4ac4ff
eduardoSousa <dudus.sousa4002@gmail.com>
2023-01-24 18:45:33 -0300

    todos os requisitos concluido

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -66,11 +66,19 @@ describe('Testando a aplicação Star Wars Search Planets', () => {
    
     await waitFor(() => expect(screen.getAllByTestId('planet-name')).toHaveLength(10));
 
+    const option = screen.getAllByRole('combobox');
+
+    userEvent.selectOptions(option[0], ['diameter'])
+
+    const valueFilter = screen.getByTestId('value-filter');
+
+    userEvent.type(valueFilter, '10000');
+
     const buttonfilter = screen.getByTestId('button-filter');
     
     userEvent.click(buttonfilter);
 
-    await waitFor(() => expect(screen.getAllByTestId('planet-name')).toHaveLength(8));
+    await waitFor(() => expect(screen.getAllByTestId('planet-name')).toHaveLength(7));
 
     const buttonDeleteFilter = screen.getByTestId('delete-one-filter');
 
@@ -81,9 +89,26 @@ describe('Testando a aplicação Star Wars Search Planets', () => {
     const removeAllFilters = screen.getByTestId('button-remove-filters'); 
 
     userEvent.click(removeAllFilters);
-  })
 
-  test('Verifica o filtro de valores numericos', async () => {
+    userEvent.selectOptions(option[0], ['diameter'])
+    userEvent.selectOptions(option[1], ['menor que'])
+    userEvent.type(valueFilter, '10000');
+    userEvent.click(buttonfilter);
+
+    await waitFor(() => expect(screen.getAllByTestId('planet-name')).toHaveLength(3));
+
+    const buttonDeleteFilter2 = screen.getByTestId('delete-one-filter');
+
+    userEvent.click(buttonDeleteFilter2);
+
+    userEvent.selectOptions(option[0], ['surface_water'])
+    userEvent.selectOptions(option[1], ['igual a'])
+    userEvent.type(valueFilter, '40');
+    userEvent.click(buttonfilter);
+
+    await waitFor(() => expect(screen.getAllByTestId('planet-name')).toHaveLength(1));
+  })
+  test('Verifica a ordenação ascendente', async () => {
     global.fetch = jest.fn().mockResolvedValue({
       status: 200,
       ok: true,
@@ -107,6 +132,35 @@ describe('Testando a aplicação Star Wars Search Planets', () => {
     userEvent.click(buttonOrdenar);
 
     const expectedPlanets = ['Yavin IV', 'Tatooine', 'Bespin', 'Endor', 'Kamino', 'Alderaan', 'Naboo', 'Coruscant'];
+    const allPlanets = screen.getAllByTestId('planet-name');
+
+    expectedPlanets.forEach((planet, index) => expect(allPlanets[index]).toHaveTextContent(planet) )
+
+  });
+  test('Verifica a ordenação descendente', async () => {
+    global.fetch = jest.fn().mockResolvedValue({
+      status: 200,
+      ok: true,
+      json: jest.fn().mockResolvedValue(result)
+    });
+    render(<App />)
+    const Loading = screen.getByTestId('loading');
+
+    await waitFor(() => waitForElementToBeRemoved(Loading), {timeout: 3000}); 
+   
+    await waitFor(() => expect(screen.getAllByTestId('planet-name')).toHaveLength(10));
+
+    const acendente = screen.getByRole('radio', {name: /descendente/i})
+
+    const buttonOrdenar = screen.getByRole('button', {
+      name: /ordenar/i
+    });
+
+    userEvent.click(acendente);
+
+    userEvent.click(buttonOrdenar);
+
+    const expectedPlanets = ['Coruscant', 'Naboo', 'Alderaan', 'Kamino', 'Endor', 'Bespin', 'Tatooine', 'Yavin IV'];
     const allPlanets = screen.getAllByTestId('planet-name');
 
     expectedPlanets.forEach((planet, index) => expect(allPlanets[index]).toHaveTextContent(planet) )
